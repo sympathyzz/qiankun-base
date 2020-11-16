2da574bfa307dcbd61ef305ccd367f15fb21ce7b
96197 <961977096@qq.com>
2020-11-16 21:10:26 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import { registerMicroApps,  start } from 'qiankun';
 
-Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+const apps=[
+  {
+    name: 'vueApp',
+    entry: '//localhost:10000',
+    container: '#vue',
+    activeRule: '/vue',
+  },
+];
+
+registerMicroApps(apps);
+
+start();
 
 new Vue({
   router,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
